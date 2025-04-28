5df2c7eea65f8a124f80bf5222b75f279799088a
feras <doonfrs@gmail.com>
2025-04-28 04:49:30 +0300

    Implement block modal for adding blocks in row view, including search functionality and UI updates for better user experience.

--- src/Http/Livewire/Row.php
+++ src/Http/Livewire/Row.php
@@ -11,6 +11,8 @@ class Row extends Component
     public array $blocks = [];
     public $availableBlocks = [];
     public ?string $rowId;
+    public bool $showBlockModal = false;
+    public string $blockFilter = '';
 
     public function mount()
     {
@@ -21,13 +23,35 @@ class Row extends Component
     {
         $blockId = uniqid();
         $this->blocks[$blockId] = ['alias' => $blockAlias];
-
+        $this->showBlockModal = false;
         $this->dispatch('block-added', $blockId);
     }
 
     public function rowSelected($rowId)
     {
         $this->dispatch('row-selected', $rowId);
+    }
+
+    public function openBlockModal()
+    {
+        $this->showBlockModal = true;
+        $this->blockFilter = '';
+    }
+
+    public function closeBlockModal()
+    {
+        $this->showBlockModal = false;
+    }
+
+    public function getFilteredBlocksProperty()
+    {
+        if (!$this->blockFilter) {
+            return $this->availableBlocks;
+        }
+        $filter = strtolower($this->blockFilter);
+        return array_values(array_filter($this->availableBlocks, function ($block) use ($filter) {
+            return str_contains(strtolower($block['label']), $filter);
+        }));
     }
 
     public function render()
