fddc203aaa6f782d4e75fc13518c785307be8bb0
feras <doonfrs@gmail.com>
2025-04-28 03:34:56 +0300

    Clean up Blade template by removing unnecessary whitespace and ensuring proper formatting.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,12 +13,14 @@
 
     <!-- Styles / Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'], 'vendor/page-builder/build')
-    
+
     @livewireStyles
 
 </head>
+
 <body class="h-screen flex flex-col bg-gray-100">
     {{ $slot }}
     @livewireScripts
 </body>
+
 </html>
