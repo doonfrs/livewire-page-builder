6ca9c0bcdac1b30d582f4961575cff5c55a27b44
feras <doonfrs@gmail.com>
2025-05-03 05:01:20 +0300

    Enhance UI and functionality in Blade templates by updating CSS classes for improved styling and responsiveness. Adjust button sizes and styles for better user interaction in row and block management components.

--- resources/views/builder-block.blade.php
+++ resources/views/builder-block.blade.php
@@ -1,9 +1,9 @@
-<div x-data="{ selected: false }">
+<div x-data="{ selected: false }" class="{{ $cssClasses }}">
     <div class="block-row relative border " :class="selected ? 'border-blue-500' : 'border-gray-300'"
         x-on:block-selected.window="selected = $event.detail.blockId == '{{ $blockId }}'"
         x-on:row-selected.window="selected = false">
-        <div class="cursor-pointer grid grid-cols-12" wire:click="blockSelected()">
-            <div class="builder-block {{ $cssClasses }}" style="pointer-events: none">
+        <div class="cursor-pointer" wire:click="blockSelected()">
+            <div class="builder-block" style="pointer-events: none">
                 @livewire($blockAlias, $properties, key($blockId . '-' . md5(json_encode($properties))))
             </div>
         </div>
