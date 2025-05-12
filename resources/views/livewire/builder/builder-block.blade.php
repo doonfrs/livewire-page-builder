0971bf6fb8933f52da4d757da5557ad17c86d21f
feras <doonfrs@gmail.com>
2025-05-12 17:44:26 +0300

    Enhance layout classes in Blade templates for improved styling consistency
    - Added new layout classes to 'safe-classes.blade.php' for better alignment options.
    - Removed 'items-center' condition from 'PageBuilderService' to ensure consistent application of layout classes.
    - Cleaned up class attributes in 'builder-block.blade.php' for streamlined styling.

--- resources/views/livewire/builder/builder-block.blade.php
+++ resources/views/livewire/builder/builder-block.blade.php
@@ -1,5 +1,5 @@
 <div x-data="{ selected: false, showContextMenu: false, x: 0, y: 0 }" 
-    class="{{ $cssClasses }} items-center"
+    class="{{ $cssClasses }}"
     style="{{ $inlineStyles }}"
     @contextmenu.prevent="
         Livewire.dispatch('show-block-context-menu', {
