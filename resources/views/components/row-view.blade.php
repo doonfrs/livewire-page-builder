993669df4a47dbf836b240f8ab4df5f81595d7b7
feras <doonfrs@gmail.com>
2025-05-12 09:40:57 +0300

    ui fixes

--- resources/views/components/row-view.blade.php
+++ resources/views/components/row-view.blade.php
@@ -1,13 +1,16 @@
-<div class="{{ $row['cssClasses'] }} group" @if (!empty($row['inlineStyles'])) style="{{ $row['inlineStyles'] }}" @endif>
-    @foreach ($row['blocks'] as $blockId => $block)
-        <div class="{{ $block['cssClasses'] }}" style="{{ $block['inlineStyles'] }}">
-            @if ($block['alias'] == 'builder-page-block')
-                @foreach ($block['rows'] as $rowId => $row)
-                    <x-page-builder::row-view :row="$row" />
-                @endforeach
-            @else
-                @livewire($block['alias'], $block['properties'], key($blockId))
-            @endif
-        </div>
-    @endforeach
+<div class="{{ $row['cssClasses'] }} group inline-block"
+    @if (!empty($row['inlineStyles'])) style="{{ $row['inlineStyles'] }}" @endif>
+    <div class="{{ $row['rowCssClasses'] }}">
+        @foreach ($row['blocks'] as $blockId => $block)
+            <div class="{{ $block['cssClasses'] }}" style="{{ $block['inlineStyles'] }}">
+                @if ($block['alias'] == 'builder-page-block')
+                    @foreach ($block['rows'] as $rowId => $row)
+                        <x-page-builder::row-view :row="$row" />
+                    @endforeach
+                @else
+                    @livewire($block['alias'], $block['properties'], key($blockId))
+                @endif
+            </div>
+        @endforeach
+    </div>
 </div>
