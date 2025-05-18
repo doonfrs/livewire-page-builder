71f27448bb3ce3f14ba6f607d754f916a39fc083
feras <doonfrs@gmail.com>
2025-05-18 11:18:34 +0300

    Add safe-classes-margins.blade.php for padding and margin utilities
    - Introduced a new Blade template to define a comprehensive set of padding and margin utility classes for various screen sizes.
    - Removed redundant padding and margin utilities from safe-classes.blade.php to streamline the codebase.
    - Enhanced maintainability by centralizing utility classes in a dedicated file.

---
+++ resources/views/dev/safe-classes-margins.blade.php
@@ -0,0 +1,57 @@
+<div style="display: none;">
+    {{--Top Padding classes --}}
+    <div class="pt-0 pt-1 pt-2 pt-3 pt-4 pt-5 pt-6 pt-7 pt-8 pt-9 pt-10 pt-11 pt-12 pt-13 pt-14 pt-15 pt-16 pt-17 pt-18 pt-19 pt-20 pt-21 pt-22 pt-23 pt-24 pt-25 pt-26 pt-27 pt-28 pt-29 pt-30"></div>
+    <div class="@3xl:pt-1 @3xl:pt-2 @3xl:pt-3 @3xl:pt-4 @3xl:pt-5 @3xl:pt-6 @3xl:pt-7 @3xl:pt-8 @3xl:pt-9 @3xl:pt-10 @3xl:pt-11 @3xl:pt-12 @3xl:pt-13 @3xl:pt-14 @3xl:pt-15 @3xl:pt-16 @3xl:pt-17 @3xl:pt-18 @3xl:pt-19 @3xl:pt-20 @3xl:pt-21 @3xl:pt-22 @3xl:pt-23 @3xl:pt-24 @3xl:pt-25 @3xl:pt-26 @3xl:pt-27 @3xl:pt-28 @3xl:pt-29 @3xl:pt-30"></div>
+    <div class="@5xl:pt-1 @5xl:pt-2 @5xl:pt-3 @5xl:pt-4 @5xl:pt-5 @5xl:pt-6 @5xl:pt-7 @5xl:pt-8 @5xl:pt-9 @5xl:pt-10 @5xl:pt-11 @5xl:pt-12 @5xl:pt-13 @5xl:pt-14 @5xl:pt-15 @5xl:pt-16 @5xl:pt-17 @5xl:pt-18 @5xl:pt-19 @5xl:pt-20 @5xl:pt-21 @5xl:pt-22 @5xl:pt-23 @5xl:pt-24 @5xl:pt-25 @5xl:pt-26 @5xl:pt-27 @5xl:pt-28 @5xl:pt-29 @5xl:pt-30"></div>
+
+    {{-- Left Padding utilities --}}
+    <div class="pl-0 pl-1 pl-2 pl-3 pl-4 pl-5 pl-6 pl-7 pl-8 pl-9 pl-10 pl-11 pl-12 pl-13 pl-14 pl-15 pl-16 pl-17 pl-18 pl-19 pl-20 pl-21 pl-22 pl-23 pl-24 pl-25 pl-26 pl-27 pl-28 pl-29 pl-30"></div>
+    <div class="@3xl:pl-1 @3xl:pl-2 @3xl:pl-3 @3xl:pl-4 @3xl:pl-5 @3xl:pl-6 @3xl:pl-7 @3xl:pl-8 @3xl:pl-9 @3xl:pl-10 @3xl:pl-11 @3xl:pl-12 @3xl:pl-13 @3xl:pl-14 @3xl:pl-15 @3xl:pl-16 @3xl:pl-17 @3xl:pl-18 @3xl:pl-19 @3xl:pl-20 @3xl:pl-21 @3xl:pl-22 @3xl:pl-23 @3xl:pl-24 @3xl:pl-25 @3xl:pl-26 @3xl:pl-27 @3xl:pl-28 @3xl:pl-29 @3xl:pl-30"></div>
+    <div class="@5xl:pl-1 @5xl:pl-2 @5xl:pl-3 @5xl:pl-4 @5xl:pl-5 @5xl:pl-6 @5xl:pl-7 @5xl:pl-8 @5xl:pl-9 @5xl:pl-10 @5xl:pl-11 @5xl:pl-12 @5xl:pl-13 @5xl:pl-14 @5xl:pl-15 @5xl:pl-16 @5xl:pl-17 @5xl:pl-18 @5xl:pl-19 @5xl:pl-20 @5xl:pl-21 @5xl:pl-22 @5xl:pl-23 @5xl:pl-24 @5xl:pl-25 @5xl:pl-26 @5xl:pl-27 @5xl:pl-28 @5xl:pl-29 @5xl:pl-30"></div>
+
+    {{-- Right Padding utilities --}}
+    <div class="pr-0 pr-1 pr-2 pr-3 pr-4 pr-5 pr-6 pr-7 pr-8 pr-9 pr-10 pr-11 pr-12 pr-13 pr-14 pr-15 pr-16 pr-17 pr-18 pr-19 pr-20 pr-21 pr-22 pr-23 pr-24 pr-25 pr-26 pr-27 pr-28 pr-29 pr-30"></div>
+    <div class="@3xl:pr-1 @3xl:pr-2 @3xl:pr-3 @3xl:pr-4 @3xl:pr-5 @3xl:pr-6 @3xl:pr-7 @3xl:pr-8 @3xl:pr-9 @3xl:pr-10 @3xl:pr-11 @3xl:pr-12 @3xl:pr-13 @3xl:pr-14 @3xl:pr-15 @3xl:pr-16 @3xl:pr-17 @3xl:pr-18 @3xl:pr-19 @3xl:pr-20 @3xl:pr-21 @3xl:pr-22 @3xl:pr-23 @3xl:pr-24 @3xl:pr-25 @3xl:pr-26 @3xl:pr-27 @3xl:pr-28 @3xl:pr-29 @3xl:pr-30"></div>
+    <div class="@5xl:pr-1 @5xl:pr-2 @5xl:pr-3 @5xl:pr-4 @5xl:pr-5 @5xl:pr-6 @5xl:pr-7 @5xl:pr-8 @5xl:pr-9 @5xl:pr-10 @5xl:pr-11 @5xl:pr-12 @5xl:pr-13 @5xl:pr-14 @5xl:pr-15 @5xl:pr-16 @5xl:pr-17 @5xl:pr-18 @5xl:pr-19 @5xl:pr-20 @5xl:pr-21 @5xl:pr-22 @5xl:pr-23 @5xl:pr-24 @5xl:pr-25 @5xl:pr-26 @5xl:pr-27 @5xl:pr-28 @5xl:pr-29 @5xl:pr-30"></div>
+
+    {{-- Bottom Padding utilities --}}
+    <div class="pb-0 pb-1 pb-2 pb-3 pb-4 pb-5 pb-6 pb-7 pb-8 pb-9 pb-10 pb-11 pb-12 pb-13 pb-14 pb-15 pb-16 pb-17 pb-18 pb-19 pb-20 pb-21 pb-22 pb-23 pb-24 pb-25 pb-26 pb-27 pb-28 pb-29 pb-30"></div> 
+    <div class="@3xl:pb-1 @3xl:pb-2 @3xl:pb-3 @3xl:pb-4 @3xl:pb-5 @3xl:pb-6 @3xl:pb-7 @3xl:pb-8 @3xl:pb-9 @3xl:pb-10 @3xl:pb-11 @3xl:pb-12 @3xl:pb-13 @3xl:pb-14 @3xl:pb-15 @3xl:pb-16 @3xl:pb-17 @3xl:pb-18 @3xl:pb-19 @3xl:pb-20 @3xl:pb-21 @3xl:pb-22 @3xl:pb-23 @3xl:pb-24 @3xl:pb-25 @3xl:pb-26 @3xl:pb-27 @3xl:pb-28 @3xl:pb-29 @3xl:pb-30"></div>
+    <div class="@5xl:pb-1 @5xl:pb-2 @5xl:pb-3 @5xl:pb-4 @5xl:pb-5 @5xl:pb-6 @5xl:pb-7 @5xl:pb-8 @5xl:pb-9 @5xl:pb-10 @5xl:pb-11 @5xl:pb-12 @5xl:pb-13 @5xl:pb-14 @5xl:pb-15 @5xl:pb-16 @5xl:pb-17 @5xl:pb-18 @5xl:pb-19 @5xl:pb-20 @5xl:pb-21 @5xl:pb-22 @5xl:pb-23 @5xl:pb-24 @5xl:pb-25 @5xl:pb-26 @5xl:pb-27 @5xl:pb-28 @5xl:pb-29 @5xl:pb-30"></div>
+
+    {{-- All Padding utilities --}}
+    <div class="p-0 p-1 p-2 p-3 p-4 p-5 p-6 p-7 p-8 p-9 p-10 p-11 p-12 p-13 p-14 p-15 p-16 p-17 p-18 p-19 p-20 p-21 p-22 p-23 p-24 p-25 p-26 p-27 p-28 p-29 p-30"></div>
+    <div class="@3xl:p-1 @3xl:p-2 @3xl:p-3 @3xl:p-4 @3xl:p-5 @3xl:p-6 @3xl:p-7 @3xl:p-8 @3xl:p-9 @3xl:p-10 @3xl:p-11 @3xl:p-12 @3xl:p-13 @3xl:p-14 @3xl:p-15 @3xl:p-16 @3xl:p-17 @3xl:p-18 @3xl:p-19 @3xl:p-20 @3xl:p-21 @3xl:p-22 @3xl:p-23 @3xl:p-24 @3xl:p-25 @3xl:p-26 @3xl:p-27 @3xl:p-28 @3xl:p-29 @3xl:p-30"></div>
+    <div class="@5xl:p-1 @5xl:p-2 @5xl:p-3 @5xl:p-4 @5xl:p-5 @5xl:p-6 @5xl:p-7 @5xl:p-8 @5xl:p-9 @5xl:p-10 @5xl:p-11 @5xl:p-12 @5xl:p-13 @5xl:p-14 @5xl:p-15 @5xl:p-16 @5xl:p-17 @5xl:p-18 @5xl:p-19 @5xl:p-20 @5xl:p-21 @5xl:p-22 @5xl:p-23 @5xl:p-24 @5xl:p-25 @5xl:p-26 @5xl:p-27 @5xl:p-28 @5xl:p-29 @5xl:p-30"></div>
+
+    {{-- Top Margin utilities --}}
+    <div class="
+        mt-0 mt-1 mt-2 mt-3 mt-4 mt-5 mt-6 mt-8 mt-10 mt-12 mt-16 mt-20 mt-24 mt-32 mt-40 mt-48 mt-56 mt-64
+        mr-0 mr-1 mr-2 mr-3 mr-4 mr-5 mr-6 mr-8 mr-10 mr-12 mr-16 mr-20 mr-24 mr-32 mr-40 mr-48 mr-56 mr-64
+        mb-0 mb-1 mb-2 mb-3 mb-4 mb-5 mb-6 mb-8 mb-10 mb-12 mb-16 mb-20 mb-24 mb-32 mb-40 mb-48 mb-56 mb-64
+        ml-0 ml-1 ml-2 ml-3 ml-4 ml-5 ml-6 ml-8 ml-10 ml-12 ml-16 ml-20 ml-24 ml-32 ml-40 ml-48 ml-56 ml-64
+    "></div>
+
+    {{-- Left Margin utilities --}}
+    <div class="ml-0 ml-1 ml-2 ml-3 ml-4 ml-5 ml-6 ml-8 ml-10 ml-12 ml-16 ml-20 ml-24 ml-32 ml-40 ml-48 ml-56 ml-64"></div>
+    <div class="@3xl:ml-1 @3xl:ml-2 @3xl:ml-3 @3xl:ml-4 @3xl:ml-5 @3xl:ml-6 @3xl:ml-7 @3xl:ml-8 @3xl:ml-9 @3xl:ml-10 @3xl:ml-11 @3xl:ml-12 @3xl:ml-13 @3xl:ml-14 @3xl:ml-15 @3xl:ml-16 @3xl:ml-17 @3xl:ml-18 @3xl:ml-19 @3xl:ml-20 @3xl:ml-21 @3xl:ml-22 @3xl:ml-23 @3xl:ml-24 @3xl:ml-25 @3xl:ml-26 @3xl:ml-27 @3xl:ml-28 @3xl:ml-29 @3xl:ml-30"></div>
+    <div class="@5xl:ml-1 @5xl:ml-2 @5xl:ml-3 @5xl:ml-4 @5xl:ml-5 @5xl:ml-6 @5xl:ml-7 @5xl:ml-8 @5xl:ml-9 @5xl:ml-10 @5xl:ml-11 @5xl:ml-12 @5xl:ml-13 @5xl:ml-14 @5xl:ml-15 @5xl:ml-16 @5xl:ml-17 @5xl:ml-18 @5xl:ml-19 @5xl:ml-20 @5xl:ml-21 @5xl:ml-22 @5xl:ml-23 @5xl:ml-24 @5xl:ml-25 @5xl:ml-26 @5xl:ml-27 @5xl:ml-28 @5xl:ml-29 @5xl:ml-30"></div>
+
+    {{-- Right Margin utilities --}}
+    <div class="mr-0 mr-1 mr-2 mr-3 mr-4 mr-5 mr-6 mr-8 mr-10 mr-12 mr-16 mr-20 mr-24 mr-32 mr-40 mr-48 mr-56 mr-64"></div>
+    <div class="@3xl:mr-1 @3xl:mr-2 @3xl:mr-3 @3xl:mr-4 @3xl:mr-5 @3xl:mr-6 @3xl:mr-7 @3xl:mr-8 @3xl:mr-9 @3xl:mr-10 @3xl:mr-11 @3xl:mr-12 @3xl:mr-13 @3xl:mr-14 @3xl:mr-15 @3xl:mr-16 @3xl:mr-17 @3xl:mr-18 @3xl:mr-19 @3xl:mr-20 @3xl:mr-21 @3xl:mr-22 @3xl:mr-23 @3xl:mr-24 @3xl:mr-25 @3xl:mr-26 @3xl:mr-27 @3xl:mr-28 @3xl:mr-29 @3xl:mr-30"></div>
+    <div class="@5xl:mr-1 @5xl:mr-2 @5xl:mr-3 @5xl:mr-4 @5xl:mr-5 @5xl:mr-6 @5xl:mr-7 @5xl:mr-8 @5xl:mr-9 @5xl:mr-10 @5xl:mr-11 @5xl:mr-12 @5xl:mr-13 @5xl:mr-14 @5xl:mr-15 @5xl:mr-16 @5xl:mr-17 @5xl:mr-18 @5xl:mr-19 @5xl:mr-20 @5xl:mr-21 @5xl:mr-22 @5xl:mr-23 @5xl:mr-24 @5xl:mr-25 @5xl:mr-26 @5xl:mr-27 @5xl:mr-28 @5xl:mr-29 @5xl:mr-30"></div>
+
+    {{-- Bottom Margin utilities --}}
+    <div class="mb-0 mb-1 mb-2 mb-3 mb-4 mb-5 mb-6 mb-8 mb-10 mb-12 mb-16 mb-20 mb-24 mb-32 mb-40 mb-48 mb-56 mb-64"></div>
+    <div class="@3xl:mb-1 @3xl:mb-2 @3xl:mb-3 @3xl:mb-4 @3xl:mb-5 @3xl:mb-6 @3xl:mb-7 @3xl:mb-8 @3xl:mb-9 @3xl:mb-10 @3xl:mb-11 @3xl:mb-12 @3xl:mb-13 @3xl:mb-14 @3xl:mb-15 @3xl:mb-16 @3xl:mb-17 @3xl:mb-18 @3xl:mb-19 @3xl:mb-20 @3xl:mb-21 @3xl:mb-22 @3xl:mb-23 @3xl:mb-24 @3xl:mb-25 @3xl:mb-26 @3xl:mb-27 @3xl:mb-28 @3xl:mb-29 @3xl:mb-30"></div>
+    <div class="@5xl:mb-1 @5xl:mb-2 @5xl:mb-3 @5xl:mb-4 @5xl:mb-5 @5xl:mb-6 @5xl:mb-7 @5xl:mb-8 @5xl:mb-9 @5xl:mb-10 @5xl:mb-11 @5xl:mb-12 @5xl:mb-13 @5xl:mb-14 @5xl:mb-15 @5xl:mb-16 @5xl:mb-17 @5xl:mb-18 @5xl:mb-19 @5xl:mb-20 @5xl:mb-21 @5xl:mb-22 @5xl:mb-23 @5xl:mb-24 @5xl:mb-25 @5xl:mb-26 @5xl:mb-27 @5xl:mb-28 @5xl:mb-29 @5xl:mb-30"></div>
+
+    {{-- Negative margin utilities --}}
+    <div class="
+        -mt-1 -mt-2 -mt-3 -mt-4 -mt-5 -mt-6 -mt-8 -mt-10 -mt-12 -mt-16 -mt-20 -mt-24 -mt-32 -mt-40 -mt-48 -mt-56 -mt-64
+        -mr-1 -mr-2 -mr-3 -mr-4 -mr-5 -mr-6 -mr-8 -mr-10 -mr-12 -mr-16 -mr-20 -mr-24 -mr-32 -mr-40 -mr-48 -mr-56 -mr-64
+        -mb-1 -mb-2 -mb-3 -mb-4 -mb-5 -mb-6 -mb-8 -mb-10 -mb-12 -mb-16 -mb-20 -mb-24 -mb-32 -mb-40 -mb-48 -mb-56 -mb-64
+        -ml-1 -ml-2 -ml-3 -ml-4 -ml-5 -ml-6 -ml-8 -ml-10 -ml-12 -ml-16 -ml-20 -ml-24 -ml-32 -ml-40 -ml-48 -ml-56 -ml-64
+    "></div>
+</div>
